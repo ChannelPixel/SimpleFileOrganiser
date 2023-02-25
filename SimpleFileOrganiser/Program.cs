a2c42ee2155ac23029a39473770e8c5b9a5f5467
Daniel Cherrin <daniel.r.cherrin@gmail.com>
2023-02-25 16:57:25 +0800

    Add project files.

---
+++ SimpleFileOrganiser/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleFileOrganiser {
+    internal class Program {
+
+        static async Task Main(string[] args) {
+            await DirectoryIsValid();
+        }
+
+        private static async Task DirectoryIsValid() {
+            bool directoryIsValid = false;
+            string directoryMessage = "Enter a valid path you want to organize :: ";
+            while (!directoryIsValid) {
+                Console.WriteLine(directoryMessage);
+                var consolePath = Console.ReadLine();
+                if (Directory.Exists(consolePath)) {
+                    directoryIsValid = await RootPathAndChildsHaveFiles(consolePath);
+                }
+                else {
+                    directoryMessage = "Directory is invalid. Enter a valid path you want to organize :: ";
+                }
+            }
+        }
+
+        private static async Task<bool> RootPathAndChildsHaveFiles(string rootPath) {
+            var files = Directory.EnumerateFiles(rootPath, "*", new EnumerationOptions() { RecurseSubdirectories = true });
+            Console.WriteLine($"{files.Count()} files found in root and subdirectories in :: {rootPath}");
+
+            return files.Count() > 0;
+        }
+    }
+}
