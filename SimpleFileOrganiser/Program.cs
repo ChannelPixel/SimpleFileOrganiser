634b0c3b7417f00b4fd6449116da2025b4336ae0
Daniel Cherrin <daniel.r.cherrin@gmail.com>
2023-02-25 18:19:38 +0800

    Update

--- SimpleFileOrganiser/Program.cs
+++ SimpleFileOrganiser/Program.cs
@@ -1,26 +1,50 @@
-﻿namespace SimpleFileOrganiser {
+﻿using System.Transactions;
+
+namespace SimpleFileOrganiser {
     internal class Program {
 
         static async Task Main(string[] args) {
-            await DirectoryIsValid();
+
+            string scanDirectoryPath = string.Empty;
+            string newDirectoryPath = string.Empty;
+            string newFolderName = string.Empty;
+
+            bool scanDirPathHasChildren = false;
+            while (!scanDirPathHasChildren) {
+                scanDirectoryPath = DirectoryIsValid("Enter a valid path you want to organize :: ");
+                scanDirPathHasChildren = RootPathAndChildsHaveFiles(scanDirectoryPath);
+            }
+
+            Console.WriteLine();
+
+            bool newFolderPathExists = false;
+            while(!Directory.Exists(newDirectoryPath)) {
+                newDirectoryPath = DirectoryIsValid("Enter a valid path you want to move the files to :: ");
+            }
+
+            while (string.IsNullOrWhiteSpace(newFolderName)) {
+                Console.WriteLine("Enter the name of the folder you would like to make :: ");
+                newFolderName = Console.ReadLine();
+            }
+
+
         }
 
-        private static async Task DirectoryIsValid() {
-            bool directoryIsValid = false;
-            string directoryMessage = "Enter a valid path you want to organize :: ";
-            while (!directoryIsValid) {
+        private static string DirectoryIsValid(string consoleMessage) {
+            string directoryMessage = consoleMessage;
+            string consolePath = string.Empty;
+            while (!Directory.Exists(consolePath)) {
                 Console.WriteLine(directoryMessage);
-                var consolePath = Console.ReadLine();
-                if (Directory.Exists(consolePath)) {
-                    directoryIsValid = await RootPathAndChildsHaveFiles(consolePath);
-                }
-                else {
-                    directoryMessage = "Directory is invalid. Enter a valid path you want to organize :: ";
+                consolePath = Console.ReadLine();
+                if (!Directory.Exists(consolePath)) {
+                    Console.WriteLine("ERROR:Invalid directory.");
                 }
             }
+
+            return consolePath;
         }
 
-        private static async Task<bool> RootPathAndChildsHaveFiles(string rootPath) {
+        private static bool RootPathAndChildsHaveFiles(string rootPath) {
             var files = Directory.EnumerateFiles(rootPath, "*", new EnumerationOptions() { RecurseSubdirectories = true });
             Console.WriteLine($"{files.Count()} files found in root and subdirectories in :: {rootPath}");
 
