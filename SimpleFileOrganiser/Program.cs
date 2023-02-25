747c7b109283ad8a4c22bb6feb85dffe040c7806
Daniel Cherrin <daniel.r.cherrin@gmail.com>
2023-02-25 20:08:33 +0800

    Good enough for what I needed

--- SimpleFileOrganiser/Program.cs
+++ SimpleFileOrganiser/Program.cs
@@ -8,6 +8,7 @@ namespace SimpleFileOrganiser {
             string scanDirectoryPath = string.Empty;
             string newDirectoryPath = string.Empty;
             string newFolderName = string.Empty;
+            int filesPerNewFolder = 30;
 
             bool scanDirPathHasChildren = false;
             while (!scanDirPathHasChildren) {
@@ -22,12 +23,42 @@ namespace SimpleFileOrganiser {
                 newDirectoryPath = DirectoryIsValid("Enter a valid path you want to move the files to :: ");
             }
 
+            Console.WriteLine();
+
             while (string.IsNullOrWhiteSpace(newFolderName)) {
                 Console.WriteLine("Enter the name of the folder you would like to make :: ");
                 newFolderName = Console.ReadLine();
             }
 
+            var files = Directory.EnumerateFiles(scanDirectoryPath, "*", new EnumerationOptions() { RecurseSubdirectories = true });
+            var existingFileInfos = files.Select(f => new FileInfo(f)).OrderBy(f => f.CreationTimeUtc).ToList();
 
+            //int subDirectorySuffix = 0;
+            string newDirectoryFolderPath = newDirectoryPath + "\\" + newFolderName;
+
+            if (!Directory.Exists(newDirectoryFolderPath)) {
+                Directory.CreateDirectory(newDirectoryFolderPath); ;
+            }
+
+            var neededFolderCount = (existingFileInfos.Count() / filesPerNewFolder) + 1;
+            var fileListIndex = 0;
+
+            for (int subIndex = 1; subIndex < neededFolderCount; subIndex++) {
+                var subDirectoryPath = newDirectoryFolderPath + "\\" + newFolderName + subIndex.ToString("000");
+
+                if (!Directory.Exists(subDirectoryPath)) {
+                    Directory.CreateDirectory(subDirectoryPath);
+                }
+
+                var fileSuffix = 0;
+                while(fileSuffix < filesPerNewFolder) {
+                    File.Copy(existingFileInfos[fileListIndex].FullName, $"{subDirectoryPath}\\{newFolderName}{fileSuffix.ToString("000")}{existingFileInfos[fileListIndex].Extension}");
+                    fileSuffix++;
+                    fileListIndex++;
+                }
+            }
+            Console.WriteLine("Bing bang boom.");
+            return;
         }
 
         private static string DirectoryIsValid(string consoleMessage) {
